8f5711f69b11e07252d83e1f22941d3189a63f5f
LitTub <jjiang40@stuy.edu>
2023-03-11 19:28:29 -0500

    changed project name

--- job-env/jobsearching/jobsearching/urls.py
+++ job-env/jobsearching/jobsearching/urls.py
@@ -1,4 +1,4 @@
-"""oursite URL Configuration
+"""jobsearching URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
