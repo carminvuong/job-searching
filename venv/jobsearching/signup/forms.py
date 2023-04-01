2ae196562ae88a47f3ab171e4c6ea227e8b54491
Jack Jiang <jjiang40@stuy.edu>
2023-04-01 13:00:34 -0400

    added logout, more fields to signup, worked on homepage

--- venv/jobsearching/signup/forms.py
+++ venv/jobsearching/signup/forms.py
@@ -12,40 +12,13 @@ class SignupForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(max_length=1000)
     last_name = forms.CharField(max_length=1000)
-    birth_date = forms.DateField(widget=DatePickerInput(
-        options={
-            "format": "mm/dd/yyyy",
-            "autoclose": True
-        }
-    ))
+    birth_date = forms.DateField(
+        label="Date", widget=DatePickerInput(), initial="2021-12-13")
 
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "start_date",
-                  "end_date",
-                  "start_time",
-                  "end_time",
-                  "start_datetime",
-                  "end_datetime",
-                  "start_month",
-                  "end_month",
-                  "start_year",
-                  "end_year",
+        fields = ["username", "first_name", "last_name", "birth_date",
                   "email", "password1", "password2"]
-        widgets = {
-            "start_date": DatePickerInput(options={"format": "MM/DD/YYYY"}),
-            "end_date": DatePickerInput(
-                options={"format": "MM/DD/YYYY"}, range_from="start_date"
-            ),
-            "start_datetime": DateTimePickerInput(),
-            "end_datetime": DateTimePickerInput(range_from="start_datetime"),
-            "start_time": TimePickerInput(),
-            "end_time": TimePickerInput(range_from="start_time"),
-            "start_month": MonthPickerInput(),
-            "end_month": MonthPickerInput(range_from="start_month"),
-            "start_year": YearPickerInput().start_of("deprecated! do not use start_of"),
-            "end_year": YearPickerInput().end_of("deprecated! do not use end_of"),
-        }
 
     def save(self, commit=True):
         user = super(SignupForm, self).save(commit=False)
