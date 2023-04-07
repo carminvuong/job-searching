ef9ae7bcf348655a9d67e162b428a7e1f34a073c
Jack Jiang <jjiang40@stuy.edu>
2023-04-07 08:51:26 -0400

    changed settings

--- jobsearching/jobsearching/settings.py
+++ jobsearching/jobsearching/settings.py
@@ -23,15 +23,16 @@ MESSAGE_TAGS = {
 }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 STATIC_URL = '/static/'
 # Directory where uploaded media is saved.
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'  # Public URL at the browser
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "jobwebsite/static"),]
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),)
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "venv\jobsearching\jobwebsite\static\images")
+MEDIA_URL = '/images/'  # Public URL at the browser
+STATICFILES_DIRS = [os.path.join(
+    BASE_DIR, "venv\jobsearching\jobwebsite\static"),]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
