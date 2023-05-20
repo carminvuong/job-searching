e5640925d8a0623186043eae3af1124581a093be
Carmin Vuong <carminvuong@gmail.com>
2023-05-20 18:37:44 -0400

    delete extar venv version

--- venv/jobsearching/jobwebsite/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobwebsiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jobwebsite'
-
-    def ready(self):
-        import jobwebsite.signals
