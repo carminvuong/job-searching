b80207c5ec825b5c20710ac4ee56a4c9a74918f1
Jack Jiang <jjiang40@stuy.edu>
2023-06-04 09:30:30 -0400

    fixed profile

--- jobsearching/jobwebsite/forms.py
+++ jobsearching/jobwebsite/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import Profile
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
 
 
 class UpdateProfile(forms.ModelForm):
--- jobsearching/jobwebsite/views.py
+++ jobsearching/jobwebsite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from . models import Job, Profile
+from . forms import UserForm, UpdateProfile
 from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
@@ -11,31 +12,9 @@ def home(request):
 
 
 def profile(request):
-    user = None
-    profile = None
-    complete = False
-    if request.user.is_authenticated:
-        user = request.user
-        try:
-            Profile.objects.get(user=user)
-        except ObjectDoesNotExist:
-            Profile.objects.create(user=user)
-
-        profile = Profile.objects.get(user=user)
-        if profile.fullname != None and profile.age != None and profile.height != None and profile.weight != None:
-            complete = True
-        if request.method == "POST":
-            form = UpdateProfile(request.POST)
-            if form.is_valid():
-                profile.fullname = form.cleaned_data["fullname"]
-                profile.age = form.cleaned_data["age"]
-                profile.height = form.cleaned_data["height"]
-                profile.weight = form.cleaned_data["weight"]
-                complete = True
-                profile.save()
-            return render(request, "jobwebsite/profile.html")
-    else:
-        return redirect("/signup/")
+    user_form = UserForm(instance=request.user)
+    profile_form = UpdateProfile(instance=request.user.profile)
+    return render(request=request, template_name=r"jobwebsite\profile.html", context={"user": request.user, "user_form": user_form, "profile_form": profile_form})
 
 
 def search(request):
